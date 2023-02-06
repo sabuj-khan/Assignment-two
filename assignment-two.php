d64efbc9e78cb2dacb7a7d831fe5f3d5be511d46
sabuj-khan <shobuj.khan1231@gmail.com>
2023-02-06 16:18:48 +0600

    Final Edition

--- assignment-two.php
+++ assignment-two.php
@@ -1,6 +1,6 @@
 <?php
 
-$tutionFee = 6999;
+$tutionFee = 2500;
 
 $commission = ( $tutionFee > 20000 ) ? "$" . ( 25 / 100 ) * $tutionFee : (  ( $tutionFee <= 20000 && $tutionFee > 10000 ) ? "$" . ( 20 / 100 ) * $tutionFee : (  ( $tutionFee <= 10000 && $tutionFee >= 7000 ) ? "$" . ( 15 / 100 ) * $tutionFee : "Commission does not suit this tuition fee." ) );
 
